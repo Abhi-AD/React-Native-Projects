0137a96e1fcf77c8e9fb8447fddfbaf6225574b3
Abhi-AD <dangiabhi332@gmail.com>
2024-06-08 18:44:40 +0545

    Footer a d WhatSucess Design

--- Bajra_COL--frontend/src/components/Footer/Footer.jsx
+++ Bajra_COL--frontend/src/components/Footer/Footer.jsx
@@ -2,80 +2,79 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/bajra_col--logo.png'
 function Footer() {
   return (
-    <footer id="bottom" data-anchor="true" className="bg-light o_footer py-5">
-      <div id="footer" className="oe_structure oe_structure_solo">
+    <footer id="bottom" data-anchor="true" className="o_footer py-5 text-light " style={{ backgroundColor: 'var(--main-color)' }}>
+      <div id="footer" className="oe_structure oe_structure_solo container">
         <section className="company_footer" data-name="Company Footer">
           <div className="container">
-            <section className="anim_footer top_footer fadeInUp row">
-              <div className="col-md-4 text-center text-md-left mb-4 mb-md-0">
+            <section className="anim_footer top_footer fadeInUp row border-bottom">
+              <div className="col-md-3 text-center text-md-left mb-3 mb-md-0 pb-3">
                 <section className="logo_wrapper">
-                  <img src={logo} alt="Logo" width="60" height="54" className="d-inline-block align-text-top" />
-
+                  <img src={logo} alt="Logo" width="90" height="70" className="d-inline-block align-text-top" />
+                  <div className="fs-5 fw-bold pt-1">
+                    <p className='fs-1 mb-0'>COL</p>
+                    <p className='mb-0'>Technology</p>
+                  </div>
                 </section>
               </div>
-              {/* <div className="vr" style="height: 250px;"></div> */}
-              <div className="col-md-4 text-center text-md-left mb-4 mb-md-0">
-                <section className="headquarter">
-                  <h5>Bajra Technologies, LLC</h5>
-                  <h5>99 Wall St.</h5>
-                  <h5>Suite 700</h5>
-                  <h5>New York, NY 10005</h5>
+              <div className="col-md-6 text-center text-md-left mb-2 mb-md-0 d-flex border-start border-end border-light ">
+                <section className="w-50 ">
+                  <p className="text-sm mb-1">Bajra Technologies, LLC</p>
+                  <p className="text-sm mb-1">99 Wall St.</p>
+                  <p className="text-sm mb-1">Suite 700</p>
+                  <p className="text-sm mb-1">New York, NY 10005</p>
+                </section>
+                <section className="w-50">
+                  <p className="text-sm mb-1">Bajra Technologies Pvt. Ltd.</p>
+                  <p className="text-sm mb-1">Charkhal, Dillibazar</p>
+                  <p className="text-sm mb-1">Kathmandu, Nepal</p>
                 </section>
               </div>
-              <div className="col-md-4 text-center text-md-left mb-4 mb-md-0">
-                <section className="regional">
-                  <h5>Bajra Technologies Pvt. Ltd.</h5>
-                  <h5>Charkhal, Dillibazar</h5>
-                  <h5>Kathmandu, Nepal</h5>
-                </section>
+              <div className="col-md-3 text-center text-md-left mb-4 mb-md-0 d-flex">
+                <div className="w-50">
+                  <section className="nav-links-2">
+                    <Link to={`/`} className="text-decoration-none d-block text-light">Home</Link>
+                    <Link to={`/about-us`} className="text-decoration-none d-block text-light ">About Us</Link>
+                    <Link to={`/what-we-do`} className="text-decoration-none d-block text-light ">Services</Link>
+                    <Link to={`/portfolio`} className="text-decoration-none d-block text-light">Our Work</Link>
+                    <Link to={`/contactus`} className="text-decoration-none d-block text-light">Contact Us</Link>
+                  </section>
+                </div>
+                <div className="w-50">
+                  <section className="nav-links-2">
+                    <Link to={`/`} className="text-decoration-none d-block text-light">Traineeship</Link>
+                    <Link to={`/about-us`} className="text-decoration-none d-block text-light">Careers</Link>
+                    <Link to={`/what-we-do`} className="text-decoration-none d-block text-light">Hardware</Link>
+                    <Link to={`/portfolio`} className="text-decoration-none d-block text-light">Workshop</Link>
+                    <Link to={`/contactus`} className="text-decoration-none d-block text-light">Blogs</Link>
+                  </section>
+                </div>
               </div>
             </section>
-            <section className="nav-links anim_footer bottom_footer fadeInUp row mt-4">
-              <div className="col-md-6 mb-4 mb-md-0">
-                <section className="nav-links-2">
-                  <Link to="/" aria-describedby="tooltip692782">Home</Link>
-                  <Link to="/about-us">About Us</Link>
-                  <Link to="/what-we-do">Services</Link>
-                  <Link to="/portfolio" aria-describedby="tooltip182503">Our Work</Link>
-                  <a href="/contactus" target="_blank">Contact Us</a>
-                </section>
-              </div>
-              <div className="col-md-6">
-                <section className="nav-links-2">
-                  <Link to="/trainee" aria-describedby="tooltip692782">Traineeship</Link>
-                  <Link to="/jobs">Careers</Link>
-                  <a href="https://hardware.bajratechnologies.com/" aria-describedby="tooltip182503">Hardware</a>
-                  <Link to="/workshop" aria-describedby="tooltip182503">Workshop</Link>
-                  <a href="https://blog.bajratechnologies.com" target="_blank">Blogs</a>
-                </section>
-              </div>
-            </section>
-            <hr />
-            <section className="anim_footer bottom_footer pt0 fadeInUp row">
+            <section className="anim_footer bottom_footer pt-3 fadeInUp row justify-content-between">
               <div className="col-md-6 text-center text-md-left mb-3 mb-md-0">
-                <div className="copyright-section">© 2024 Bajra Technologies LLC</div>
+                <div className="copyright-section">© 2024 COL Technologies LLC</div>
               </div>
               <div className="col-md-6 text-center text-md-right">
-                <div className="social-section">
-                  <a target="_blank" href="https://www.facebook.com/bajratechnologies">
+                <div className="social-section fs- d-flex justify-content-end">
+                  <Link to={`/`} className=" text-white me-4">
                     <i className="bi bi-facebook"></i>
-                  </a>
-                  <Link target="_blank" to={`/`}>
+                  </Link>
+                  <Link to={`/`} className="text-white me-4">
                     <i className="bi bi-linkedin"></i>
                   </Link>
-                  <Link target="_blank" to={`/`}>
+                  <Link to={`/`} className="text-white me-4">
                     <i className="bi bi-twitter"></i>
                   </Link>
-                  <Link target="_blank" to={`/`}>
+                  <Link to={`/`} className=" text-white me-4">
                     <i className="bi bi-instagram"></i>
                   </Link>
                 </div>
               </div>
             </section>
-          </div>
-        </section>
-      </div>
-    </footer>
+          </div >
+        </section >
+      </div >
+    </footer >
   );
 }
 
--- Bajra_COL--frontend/src/container/WhatDoSucess/WhatDoSucess.jsx
+++ Bajra_COL--frontend/src/container/WhatDoSucess/WhatDoSucess.jsx
@@ -1,14 +1,113 @@
+import { Link } from "react-router-dom";
+import './WhatDoSucess.css';
 
 const WhatDoSucess = () => {
      return (
-          <div className="bg-light">
-               <div className="container text-center  my-5 w-50">
-                    <p className="fs-3" style={{ color: 'var(--main-color)' }}>We Ship Success Everyday</p>
-                    <p>What Can We Do For You?</p>
-                    <p>Bajra excels at delivering innovative technological solutions for all types of technology needs across various domains. Regardless of the complexity of your ideas or solutions, we meticulously analyze every potential path and select the best course of action to craft the optimal version of your dream project within required constraints. We don&apos;t hesitate to push the limits using the latest technologies or explore the bleeding edge and we are also very good at keeping things stable and working within specified processes or existing technology stacks. Here are some of the major categories of services we provide for our beloved partners.</p>
+          <div style={{ backgroundColor: 'var(--secondary-color)' }}>
+               <div className="p-5">
+                    <div className="container text-center my-5 w-50">
+                         <p className="fs-2" style={{ color: 'var(--main-color)' }}>We Ship Success Everyday</p>
+                         <p className="fs-1">What Can We Do For You?</p>
+                         <p className="fs-5">
+                              Bajra excels at delivering innovative technological solutions for all types of technology needs across various domains.
+                              Regardless of the complexity of your ideas or solutions, we meticulously analyze every potential path and select the best
+                              course of action to craft the optimal version of your dream project within required constraints. We don&apos;t hesitate to push
+                              the limits using the latest technologies or explore the bleeding edge and we are also very good at keeping things stable
+                              and working within specified processes or existing technology stacks. Here are some of the major categories of services
+                              we provide for our beloved partners.
+                         </p>
+                    </div>
+                    <div className="container">
+                         <div className="row justify-content-center row-cols-1 row-cols-md-2 row-cols-lg-5 g-4 mx-auto">
+                              {services.map((service, index) => (
+                                   <div className="col d-flex" key={index}>
+                                        <div className="card border-0 shadow card-hover h-100 w-100 d-flex flex-column">
+                                             <div className="default-content flex-grow-1 d-flex flex-column">
+                                                  <img src={service.image} alt={service.alt} width={50} className="mx-auto pt-3" />
+                                                  <div className="card-body flex-grow-1 d-flex flex-column">
+                                                       <p className="card-title fs-6">{service.title}</p>
+                                                  </div>
+                                             </div>
+                                             <div className="hover-content h-80 flex-grow-1 d-flex flex-column">
+                                                  <div className="card-body flex-grow-1 d-flex flex-column">
+                                                       <p className="card-title fs-6">{service.description}</p>
+                                                       <Link to={`/what-we-do`} type="button" className="btn btn-outline-light btn-hover-dark border-0 mt-auto" data-mdb-ripple-init data-mdb-ripple-color="dark">
+                                                            Learn More
+                                                            <i className="bi bi-arrow-right" data-aos="fade-left"></i>
+                                                       </Link>
+                                                  </div>
+                                             </div>
+                                        </div>
+
+                                   </div>
+                              ))}
+                         </div>
+                    </div>
+                    <div className="text-center mt-5">
+                         <Link style={{ backgroundColor: 'var(--main-color)' }} type="button" className="text-light btn fs-4" to={`/what-we-do`}>
+                              What We Do <i className="bi bi-arrow-right" data-aos="fade-left"></i>
+                         </Link>
+                    </div>
                </div>
           </div>
-     )
+     );
 }
 
-export default WhatDoSucess
+const services = [
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/architecture.png",
+          alt: "Technical Architecture and Platform Design",
+          title: "Technical Architecture and Platform Design",
+          description: "Our first priority for delivering solutions with flawless execution is to create a solid.."
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/user-experience-design.png",
+          alt: "User Experience/ Interaction Principles and Design",
+          title: "User Experience/ Interaction Principles and Design",
+          description: "Every business-be it Bajra or any of our clients- builds software and programs to.."
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/agile.png",
+          alt: "Agile Software Prototyping and Development",
+          title: "Agile Software Prototyping and Development",
+          description: "All projects that we undertake follow agile prototyping and development, which"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/project_management.png",
+          alt: "Project Management Suport and Services",
+          title: "Project Management Suport and Services",
+          description: "Our agile development strategy is effective because it stands on the shoulders of our"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/consultation.png",
+          alt: "Consulting and Advisory Services",
+          title: "Consulting and Advisory Services",
+          description: "Bajra is home to diverse experts in different fields required to develop software solutions"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/support_maintainance.png",
+          alt: "Support and Maintenance Services",
+          title: "Support and Maintenance Services",
+          description: "Once you start with Bajra, we will deliver as your closest partner from beginning to end"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/continuous_delivery.png",
+          alt: "Continuous Delivery and Code Deployment",
+          title: "Continuous Delivery and Code Deployment",
+          description: "Continuous Delivery is an important development practice where we focus on"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/software_monitoring.png",
+          alt: "Software Monitoring and Analytics",
+          title: "Software Monitoring and Analytics",
+          description: "We understand the critical importance of monitoring software performance, user"
+     },
+     {
+          image: "https://bajratechnologies.com/bajra_snippets/static/src/img/hardware_software.png",
+          alt: "Custom Hardware and Software",
+          title: "Custom Hardware and Software",
+          description: "At Bajra, we take pride in our expertise in crafting Custom Hardware and Software solutions that"
+     },
+];
+
+export default WhatDoSucess;
