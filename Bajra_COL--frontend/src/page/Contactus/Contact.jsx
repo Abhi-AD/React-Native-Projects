b2debba74c4a981efd99a973e4fff4b6fc0235ca
Abhi-AD <dangiabhi332@gmail.com>
2024-06-30 15:09:02 +0545

    Images color change

--- Bajra_COL--frontend/src/page/Contactus/Contact.jsx
+++ Bajra_COL--frontend/src/page/Contactus/Contact.jsx
@@ -69,11 +69,16 @@ const Contact = () => {
               height="500"
               frameBorder="0"
               style={{ border: 0, borderRadius: '10px' }}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.9495476371276!2d85.32065841506192!3d27.706784382794464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198a307baabf%3A0x4f874c00e95ac829!2sP84G%2BP57%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1624525641845!5m2!1sen!2snp"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d220.740621054911!2d85.3204025257862!3d27.721919200155398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1719737568336!5m2!1sen!2snp"
               allowFullScreen
               title="Map"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             >
             </iframe>
+
+
+
 
             <div className="col-md-8">
               <div className='d-flex'>
