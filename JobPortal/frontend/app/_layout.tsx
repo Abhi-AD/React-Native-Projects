71c3af7ef01e8cb7a80f498d2a904e87f4b5903f
Abhi-AD <dangiabhi332@gmail.com>
2024-09-09 23:14:20 +0545

    JobPortal:Frontend:LoginPage Setup

--- JobPortal/frontend/app/_layout.tsx
+++ JobPortal/frontend/app/_layout.tsx
@@ -1,26 +1,21 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { StatusBar } from "expo-status-bar";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     "Telma-Regular": require("../assets/fonts/Telma-Regular.ttf"),
     "Telma-Bold": require("../assets/fonts/Telma-Bold.ttf"),
     "Telma-Medium": require("../assets/fonts/Telma-Medium.ttf"),
     "Telma-Black": require("../assets/fonts/Telma-Black.ttf"),
+    "Neco-Regular": require("../assets/fonts/Neco-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -34,11 +29,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      <StatusBar style="dark" />
     </ThemeProvider>
   );
 }
